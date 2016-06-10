de1be61a38ee225a958b5aaaab7822228c3aa977
cpp.rakesh <cpp.rakesh@gmail.com>
2016-06-10 08:47:41 +0100

    Solution for Merge Sort

--- chapter_2/Test.cpp
+++ chapter_2/Test.cpp
@@ -1,15 +1,17 @@
 /*
   Introduction to Algorithms, CLRS
   Chapter 2, Getting Started
-  Test case for Insertion sort
+  Test case for Insertion sort and Merge Sort
   Author : Rakesh Kumar @ cpp.rakesh@gmail.com
   Date : June 10th, 2016
  */
 
+#include "MergeSort.h"
 #include "InsertionSort.h"
 #include "../Common/Common.h"
 
-int main() {
+
+void test_insertion_sort() {
 	int A[] = {98, 12, 72, 123, 1, 8, 282, 3842, 8, 783, 23, 72, 17, 48, 52, 81, 73, 134, 84, 72, 841, 13, 85, 232, 4, 272};
 	std::vector<int> V(A, A + sizeof(A) / sizeof(A[0]));
 
@@ -19,7 +21,22 @@ int main() {
 	sort.Sort(V);
 	common.PrintVector(V);
 	sort.SortReverse(V);
+	common.PrintVector(V);	
+}
+
+void test_merge_sort() {
+	int A[] = {98, 12, 72, 123, 1, 8, 282, 3842, 8, 783, 23, 72, 17, 48, 52, 81, 73, 134, 84, 72, 841, 13, 85, 232, 4, 272};
+	std::vector<int> V(A, A + sizeof(A) / sizeof(A[0]));
+
+	Common<int> common;
 	common.PrintVector(V);
-	
+	MergeSort<int> sort;
+	sort.Sort(V);
+	common.PrintVector(V);
+}
+
+int main() {
+	//test_insertion_sort();
+	test_merge_sort();
 	return 0;
 }
