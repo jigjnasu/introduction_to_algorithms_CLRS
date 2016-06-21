32d9ab043db98f4df265f35846ebec5b62f1adab
cpp.rakesh <cpp.rakesh@gmail.com>
2016-06-21 03:48:58 +0100

    Adding questions

--- Chapter_2_Getting_Started/2.2_Analyzing_Algorithms/Exercises/2.2-2/SelectionSort.h
+++ Chapter_2_Getting_Started/2.2_Analyzing_Algorithms/Exercises/2.2-2/SelectionSort.h
@@ -16,6 +16,17 @@
   Worst Case will be same as N ^ 2 as well.
  */
 
+/*
+  2.2-2
+  Consider sorting n numbers stored in array A by first finding the smallest element
+  of A and exchanging it with the element in A[1]. Then find the second smallest
+  element of A, and exchange it with A[2]. Continue in this manner for the first n - 1
+  elements of A. Write pseudocode for this algorithm, which is known as selection
+  sort. What loop invariant does this algorithm maintain? Why does it need to run
+  for only the first n - 1 elements, rather than for all n elements? Give the best-case
+  and worst-case running times of selection sort in ‚-notation.
+ */
+
 #ifndef DATA_STRUCTURE_CHAPTER_2_2_2_2_SELECTION_SORT_H_
 #define DATA_STRUCTURE_CHAPTER_2_2_2_2_SELECTION_SORT_H_
 
