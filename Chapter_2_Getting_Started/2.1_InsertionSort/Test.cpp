00c2974121fd823b47f21fa7b39b49fe784d26bb
cpp.rakesh <cpp.rakesh@gmail.com>
2016-06-21 03:17:48 +0100

    Adding test cases

--- Chapter_2_Getting_Started/2.1_InsertionSort/Test.cpp
+++ Chapter_2_Getting_Started/2.1_InsertionSort/Test.cpp
@@ -1,21 +1,24 @@
 /*
   Introduction to Algorithms, CLRS
   Chapter 2, Getting Started
-  Test case for Insertion sort and Merge Sort
+  Test case for Insertion Sort implementation
   Author : Rakesh Kumar @ cpp.rakesh@gmail.com
   Date : June 10th, 2016
  */
 
-#include "MergeSort.h"
+#include "InsertionSort.h"
 #include "../../Common/Common.h"
+#include <cstdio>
+#include <vector>
 
 int main() {
-	int A[] = {98, 12, 72, 123, 1, 8, 282, 3842, 8, 783, 23, 72, 17, 48, 52, 81, 73, 134, 84, 72, 841, 13, 85, 232, 4, 272};
+	int A[] = {9, 738, 27, 23, 958, 37, 34, 274, 82, 16, 38, 163, 45, 82, 163, 858, 1, 7, 73, 17, 12, 74, 234, 721, 23};
 	std::vector<int> V(A, A + sizeof(A) / sizeof(A[0]));
 
 	Common<int> common;
 	common.PrintVector(V);
-	MergeSort<int> sort;
+
+	InsertionSort<int> sort;
 	sort.Sort(V);
 	common.PrintVector(V);
 	
