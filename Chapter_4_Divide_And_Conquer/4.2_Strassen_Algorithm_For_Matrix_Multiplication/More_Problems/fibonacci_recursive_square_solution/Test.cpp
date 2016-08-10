913f60d3e9544d5351fdb20b3ee60bd4d435e2f7
cpp-rakesh <cpp.rakesh@gmail.com>
2016-08-11 02:37:55 +1000

    Recursive Squaring Algorithm, mathematical induction and implmentation

---
+++ Chapter_4_Divide_And_Conquer/4.2_Strassen_Algorithm_For_Matrix_Multiplication/More_Problems/fibonacci_recursive_square_solution/Test.cpp
@@ -0,0 +1,21 @@
+#include "Fibonacci.h"
+
+void print(const int (&A)[2][2]) {
+	for (int i = 0; i < 2; ++i) {
+		for (int j = 0; j < 2; ++j)
+			printf("%d ", A[i][j]);
+		printf("\n");
+	}
+}
+
+int main() {
+	for (int i = 1; i <= 30; i += 3) {
+		Fibonacci<int> fibo;
+		int result[2][2] = {0};	
+		fibo.Calculate(i, result);
+		printf("%d %d %d ", result[1][1], result[1][0], result[0][0]);
+	}
+	printf("\n");
+	
+	return 0;
+}
