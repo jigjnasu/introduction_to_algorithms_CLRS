99b313d5c606e57135199a1322fd54642e717b36
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-07-13 01:11:30 +1000

    restructure

--- chapter_4_divide_and_conquer/4.1_the_maximum_subArray_problem/test.cpp
+++ chapter_4_divide_and_conquer/4.1_the_maximum_subArray_problem/test.cpp
@@ -1,27 +1,26 @@
-#include "MaxSubArrayIterative.h"
-#include "MaxSubArray.h"
+#include "max_sub_array_iterative.h"
+#include "max_sub_array.h"
 #include <cstdio>
 
 void print_vector(const std::vector<int>& V) {
 	printf("-----------------------------------------------------------\n");
 	for (std::size_t i = 0; i < V.size(); ++i)
 		printf("%d ", V[i]);
-	printf("\n-----------------------------------------------------------\n");	
+	printf("\n-----------------------------------------------------------\n");
 }
 
 int main() {
-	int A[] = {-9, 19, 27, -92, 17, 28, 1, 72, 38, -3, -3, -45, 34, 93, 72, -8, -81, -83, 12};
-	std::vector<int> V(A, A + sizeof(A) / sizeof(A[0]));
+    std::vector<int> v = {-9, 19, 27, -92, 17, 28, 1, 72, 38, -3, -3, -45, 34, 93, 72, -8, -81, -83, 12};
 
-	int sum = V[0];
+	int sum = v[0];
 	int start = 0;
 	int end = 0;
-	
-	MaxSubArrayIterative<int> max_iterative;
-	max_iterative.Max(V, sum, start, end);
 
-	print_vector(V);
-	
+	MaxSubArrayIterative<int> max_iterative;
+	max_iterative.Max(v, sum, start, end);
+
+	print_vector(v);
+
 	printf("--------------------------- iterative ------------------------------\n");
 	printf("Maximum sub array sum from [%d] to [%d] == [%d]\n", start, end, sum);
 	printf("--------------------------- iterative ------------------------------\n");
@@ -30,8 +29,8 @@ int main() {
 
 	MaxSubArray<int> max;
 	printf("------------------------- Divide and Conquer -----------------------\n");
-	printf("Maximum sub array sum == [%d]\n", max.Max(V));
-	printf("------------------------- Divide and Conquer -----------------------\n");	
-	
+	printf("Maximum sub array sum == [%d]\n", max.Max(v));
+	printf("------------------------- Divide and Conquer -----------------------\n");
+
 	return 0;
 }
