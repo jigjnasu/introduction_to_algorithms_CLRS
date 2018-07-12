e18e80eaa5cc155071a07ec126a572bbe23bc9fb
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-07-13 01:09:15 +1000

    resturcture

--- chapter_4_divide_and_conquer/4.1_the_maximum_subArray_problem/exercises/4.1-5/test.cpp
+++ chapter_4_divide_and_conquer/4.1_the_maximum_subArray_problem/exercises/4.1-5/test.cpp
@@ -1,23 +1,21 @@
-#include "MaxSubArray.h"
+#include "max_sub_array.h"
 #include <cstdio>
 
 void print_vector(const std::vector<int>& V) {
 	printf("-------------------------------------------------------------------------\n");
 	for (std::size_t i = 0; i < V.size(); ++i)
 		printf("%d ", V[i]);
-	printf("\n-------------------------------------------------------------------------\n");	
+	printf("\n-------------------------------------------------------------------------\n");
 }
 
 int main() {
-	int A[] = {-58, 77, -12, -94, -38, 75, 98, 53};
-	std::vector<int> V(A, A + sizeof(A) / sizeof(A[0]));
-
-	print_vector(V);
+    std::vector<int> v = {-58, 77, -12, -94, -38, 75, 98, 53};
+	print_vector(v);
 
 	MaxSubArray<int> max;
 	printf("-------------------------------------------------------------------------\n");
-	printf("Maximum sub array == [%d]\n", max.Max(V));
-	printf("-------------------------------------------------------------------------\n");	
-	
+	printf("Maximum sub array == [%d]\n", max.Max(v));
+	printf("-------------------------------------------------------------------------\n");
+
 	return 0;
 }
