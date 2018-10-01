64ced955af36bcb45a5078cae85733d1458d74c4
Rakesh Kumar <cpp.rakesh@gmail.com>
2018-10-02 00:37:03 +0800

    c++ 11 chrono update

--- chapter_4_divide_and_conquer/4.1_the_maximum_subArray_problem/exercises/4.1-3/repo/test.cpp
+++ chapter_4_divide_and_conquer/4.1_the_maximum_subArray_problem/exercises/4.1-3/repo/test.cpp
@@ -2,65 +2,66 @@
 #include "max_sub_array_iterative.h"
 #include <cstdio>
 #include <cstdlib>
-#include <ctime>
+#include <chrono>
 
 void print_vector(const std::vector<int>& V) {
-	printf("-----------------------------------------------------------\n");
-	for (std::size_t i = 0; i < V.size(); ++i)
-		printf("%d ", V[i]);
-	printf("\n-----------------------------------------------------------\n");
+    printf("-----------------------------------------------------------\n");
+    for (std::size_t i = 0; i < V.size(); ++i)
+        printf("%d ", V[i]);
+    printf("\n-----------------------------------------------------------\n");
 }
 
 void generate(std::vector<int>& V, const int& n) {
-	const int start = -100;
-	const int end = 100;
-	for (int i = 0; i < n; ++i) {
-		int value = rand() % (end - start + 1) + start;
-		V.push_back(value);
-	}
+    const int start = -100;
+    const int end = 100;
+    for (int i = 0; i < n; ++i) {
+        int value = rand() % (end - start + 1) + start;
+        V.push_back(value);
+    }
 }
 
 void max_dnc(const std::vector<int>& V) {
-	MaxSubArray<int> max;
-	printf("Max sub array sum Divide and Conquer    == [%d]\n", max.Max(V));
+    MaxSubArray<int> max;
+    printf("Max sub array sum Divide and Conquer    == [%d]\n", max.Max(V));
 }
 
 void max_iterative(const std::vector<int>& V) {
-	MaxSubArrayIterative<int> max;
+    MaxSubArrayIterative<int> max;
 
-	int sum = 0;
-	int start = V[0];
-	int end = 0;
-	max.Max(V, sum, start, end);
-	printf("Max sub array sum from [%4d] to [%4d] == [%d]\n", start, end, sum);
+    int sum = 0;
+    int start = V[0];
+    int end = 0;
+    max.Max(V, sum, start, end);
+    printf("Max sub array sum from [%4d] to [%4d] == [%d]\n", start, end, sum);
 }
 
 int main() {
-	int n = 2;
-	while (1) {
-		std::vector<int> V;
-		generate(V, n);
+    int n = 2;
+    while (1) {
+        std::vector<int> V;
+        generate(V, n);
 
-		//print_vector(V);
+        //print_vector(V);
 
-		std::clock_t start_d = clock();
-		max_dnc(V);
-		std::clock_t end_d = clock();
+        using clock = std::chrono::steady_clock;
+        clock::time_point start_d = clock::now();
+        max_dnc(V);
+        clock::time_point end_d = clock::now();
 
-		std::clock_t start_i = clock();
-		max_iterative(V);
-		std::clock_t end_i = clock();
+        clock::time_point start_i = clock::now();
+        max_iterative(V);
+        clock::time_point end_i = clock::now();
 
-		printf("-----------------------------------------------------------------------------\n");
-		printf("N == [%d] || Divide and Concquer == [%.8f] seconds || Iterative == [%.8f] seconds\n",
-			   n, (end_d - static_cast<double>(start_d)), (end_i - static_cast<double>(start_i)));
-		printf("-----------------------------------------------------------------------------\n");
+        printf("-----------------------------------------------------------------------------\n");
+        printf("N == [%d] || Divide and Concquer == [%16lf] seconds || Iterative == [%16lf] seconds\n",
+               n, (end_d - start_d, (end_i - start_i).count()));
+        printf("-----------------------------------------------------------------------------\n");
 
-		if ((end_i - start_i) > (end_d - start_d))
-			break;
+        if ((end_i - start_i) > (end_d - start_d))
+            break;
 
-		++n;
-	}
+        ++n;
+    }
 
-	return 0;
+    return 0;
 }
